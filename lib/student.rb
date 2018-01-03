b88461fc9a89cb382a18d9ae341d042054b98b44
Chris Gullickson <cgullickson5@gmail.com>
2018-01-03 22:24:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,12 +74,12 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(X)
     sql = <<-SQL
     SELECT *
     FROM students
     WHERE grade = 10
-    LIMIT ?
+    LIMIT X
     SQL
 
     DB[:conn].execute(sql)
